19506acc63f4bc178d169acc35373d2884b14a34
XAdrien75 <77054356+XAdrien75@users.noreply.github.com>
2024-06-14 14:20:50 +0200

    [FEAT] init router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
-<script>
-import NavBar from './components/NavBar.vue';
-
-export default {
-  name: 'HelloWorld',
-  components: {
-    NavBar,
-  },
-};
-</script>
-
 <template>
-  <NavBar />
-  <div class="relative">
-    <div class="absolute inset-0 bg-secondary-color opacity-50">
-    </div>
-    <img src="@/assets/images/convention.jpg" alt="Background Image"
-    class="w-full h-80vh object-cover parallax"/>
-  </div>
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
