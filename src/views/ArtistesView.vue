058c7136409bc74bd2817b05070c2c30058f671a
XAdrien75 <77054356+XAdrien75@users.noreply.github.com>
2024-06-21 19:32:07 +0200

    [FEAT] Maj page artistes; create page Rubriquecosplay

--- src/views/ArtistesView.vue
+++ src/views/ArtistesView.vue
@@ -1,5 +1,23 @@
+<script>
+  import CardInfo from '../components/CardInfo.vue';
+
+  export default {
+    name: 'HelloWorld',
+    components: {
+      CardInfo
+    }
+  };
+</script>
 <template>
-    <div class="artistes">
-      <h1 class="text-2xl">Artistes</h1>
+  <div class="max-w-[1440px] m-auto p-4 md:pb-20 pt-8 md:px-[6rem]">
+    <h2 class="pb-[2rem] text-3xl font-bold">Nos artistes</h2>
+    <p class="pb-[2rem]">Lorem ipsum</p>
+
+    <div class="gap-12 md:p-0 md:flex">
+      <CardInfo title="Danaé cosplay" imgUrl="https://i.lepelerin.com/1400x787/smart/2022/10/04/femmes-artistes.jpg" :hasPopup="true"/>
+      <CardInfo title="Danaé cosplay" imgUrl="https://gamingway.fr/wp-content/uploads/2023/07/cosplay-Japan-Expo-2023-Genshin-image-en-une.jpg" :hasPopup="true"/>
+      <CardInfo title="Danaé cosplay" imgUrl="https://gamingway.fr/wp-content/uploads/2023/07/cosplay-Japan-Expo-2023-Genshin-image-en-une.jpg" :hasPopup="true"/>
+      <CardInfo title="Danaé cosplay" imgUrl="https://gamingway.fr/wp-content/uploads/2023/07/cosplay-Japan-Expo-2023-Genshin-image-en-une.jpg" :hasPopup="true"/>
     </div>
+  </div>
   </template>
--- src/views/RubriquecosplayView.vue
+++ src/views/RubriquecosplayView.vue
@@ -13,7 +13,7 @@ export default {
     sagittis bibendum sapien. Integer molestie nulla eget placerat auctor. Lorem ipsum dolor sit amet,
     consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac, sagittis bibendum sapien.
     Integer molestie nulla eget placerat auctor."/>
-    <ImageTextGrid :isInverted="true" title="Mon titre" paragraphe="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    <ImageTextGrid :isInverted="true" title="Mon titre 2" paragraphe="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
     sagittis bibendum sapien. Integer molestie nulla eget placerat auctor. Lorem ipsum dolor sit amet,
     consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac, sagittis bibendum sapien.
     Integer molestie nulla eget placerat auctor."/>
