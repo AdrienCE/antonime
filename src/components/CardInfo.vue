1b2bf4dc96156cc4f8991351f2f020f8879f63f7
XAdrien75 <77054356+XAdrien75@users.noreply.github.com>
2024-06-22 16:27:52 +0200

    [MAJ] arrondis full cards

--- src/components/CardInfo.vue
+++ src/components/CardInfo.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="md:w-1/4 relative">
     <a v-if="displayMode === 'link'" :href="url">
-      <img class="md:h-[260px] object-cover cursor-pointer" :src="imgUrl" alt="Card image">
+      <img class="md:h-[260px] object-cover cursor-pointer rounded-md" :src="imgUrl" alt="Card image">
       <h3 class="py-4">{{ title }}</h3>
     </a>
     <div v-else class="md:h-[200px] object-cover cursor-pointer" @click="handleClick">
       <div v-if="displayMode === 'custom'" class="md:w-[300px] md:h-[120px] bg-white p-6 shadow-2xl rounded-lg md:flex">
         <img :src="imgUrl" alt="Card image" class="md:h-[80px] md:w-[80px] object-cover md:rounded-full">
       </div>
-      <img v-else :src="imgUrl" alt="Card image" class="md:h-[260px] object-cover">
+      <img v-else :src="imgUrl" alt="Card image" class="md:h-[260px] object-cover rounded-md">
       <h3 class="py-4">{{ title }}</h3>
     </div>
 
