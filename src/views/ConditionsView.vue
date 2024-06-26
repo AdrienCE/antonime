c87d2e9b03ac9b937878386dad063bed516bab6c
Adrien <adrien.cenee@outlook.fr>
2024-06-26 18:38:49 +0200

    [MAJ] structure conditions + mentions

--- src/views/ConditionsView.vue
+++ src/views/ConditionsView.vue
@@ -1,11 +1,33 @@
 <template>
   <HeaderTitle title="CONDITIONS GENERALES" />
   
-  <div class="container m-auto my-7 px-7 md:px-0">
-    <h2 class="pb-[2rem] text-3xl font-bold">Conditions générales de vente - Internet - En vigueur au 01/06/2024</h2>
-     <!-- A remplir   -->
-  </div>
-</template>
+  <div class="container m-auto my-14 px-7 md:px-0">
+      <h2 class="pb-[2rem] text-3xl font-bold">Conditions générales de vente - Internet - <span class="text-sm font-normal">En vigueur au 01/06/2024</span></h2>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 1 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 2 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 3 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 4 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    </div>
+  </template>
 
 <script>
   import HeaderTitle from '../components/HeaderTitle.vue';
--- src/views/MentionsLegalesView.vue
+++ src/views/MentionsLegalesView.vue
@@ -1,9 +1,31 @@
 <template>
     <HeaderTitle title="MENTION LEGALES" />
     
-    <div class="container m-auto my-7 px-7 md:px-0">
-      <h2 class="pb-[2rem] text-3xl font-bold">Mentions légales - En vigueur au 01/06/2024</h2>
-       <!-- A remplir   -->
+    <div class="container m-auto my-14 px-7 md:px-0">
+      <h2 class="pb-[2rem] text-3xl font-bold">Mentions légales - <span class="text-sm font-normal">En vigueur au 01/06/2024</span></h2>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 1 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 2 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 3 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
+    <h3 class="text-2xl font-bold py-8">ARTICLE 4 - L'EDITEUR</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien.</p>
     </div>
   </template>
   
