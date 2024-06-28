8b009b48d4628d2659413a56e3c91e6b7754443c
Nico <dbz0793@gmail.com>
2024-06-28 20:53:42 +0200

    [FEAT] -- Remove useless folder

--- src/views/ExposantsView.vue
+++ src/views/ExposantsView.vue
@@ -3,7 +3,7 @@
 
   <div class="container m-auto my-7 px-7 md:px-0">
     <h2 class="pb-[2rem] text-3xl font-bold">Exposants</h2>
-    <div class="overflow-x-auto whitespace-nowrap inline-box xl:inline-grid xl:grid-cols-3 xl:w-full xl:gap-12">
+    <div class="overflow-x-auto whitespace-nowrap inline-box xl:inline-grid xl:grid-cols-5 xl:w-full xl:gap-12">
       <CardInfo title="Danaé cosplay" imgUrl="G-01.webp" :hasPopup="true" displayMode="custom" listeInLine="true" />
       <CardInfo title="Danaé cosplay" imgUrl="G-01.webp" :hasPopup="true" displayMode="custom" listeInLine="true" />
       <CardInfo title="Danaé cosplay" imgUrl="G-01.webp" :hasPopup="true" displayMode="custom" listeInLine="true" />
