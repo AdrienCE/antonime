a9c5ccb5ec5e00e73cf4758483196dc11273ce02
Adrien <adrien.cenee@outlook.fr>
2024-06-28 12:52:11 +0200

    [MAJ] text conditions + mentions

--- src/views/ConditionsView.vue
+++ src/views/ConditionsView.vue
@@ -2,50 +2,122 @@
   <HeaderTitle title="CONDITIONS GENERALES" />
   
   <div class="container m-auto my-14 px-7 md:px-0">
-    <h2 class="pb-[2rem] text-3xl font-bold">Conditions générales de vente - Internet - <span
-        class="text-sm font-normal">En vigueur au 01/06/2024</span></h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
-      sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum
-      nec tempus ac,
-      sagittis bibendum sapien.</p>
+    <h2 class="pb-[2rem] text-3xl font-bold">
+      Conditions générales de vente - Internet - 
+      <span class="text-sm font-normal">En vigueur au 01/06/2024</span>
+    </h2>
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 1 - CHAMP D’APPLICATION</h3>
+    <p>Les présentes Conditions Générales de Ventes (dites CGV) s’appliquent, sans restriction ni
+      réserve à tout achat des services suivants :</p>
     <br>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
-      sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum
-      nec tempus ac,
-      sagittis bibendum sapien.</p>
-    <h3 class="text-2xl font-bold py-8">ARTICLE 1 - L'EDITEUR</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
-      sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum
-      nec tempus ac,
-      sagittis bibendum sapien.</p>
-    <h3 class="text-2xl font-bold py-8">ARTICLE 2 - L'EDITEUR</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
-      sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum
-      nec tempus ac,
-      sagittis bibendum sapien.</p>
-    <h3 class="text-2xl font-bold py-8">ARTICLE 3 - L'EDITEUR</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
-      sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum
-      nec tempus ac,
-      sagittis bibendum sapien.</p>
-    <h3 class="text-2xl font-bold py-8">ARTICLE 4 - L'EDITEUR</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
-      sagittis bibendum sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum
-      nec tempus ac,
-      sagittis bibendum sapien.</p>
+    <p>
+      - Billetterie pour une entrée
+      <br>
+      - Accès événement Maid Café
+    </p>
+    <br>
+    <p>Tel quel proposé par le prestataire aux clients non professionnels (“les clients” ou “le client”)
+      sur le site https://antonime.fr</p>
+    <br>
+    <p>Les caractéristiques principales des services sont présentées sur le site internet
+      https://antonime.fr</p>
+    <br>
+    <p>Le client est tenu d’en prendre connaissance avant toute passation de commande. Le choix
+      et l’achat d’un service est de la seule responsabilité du client. Ces CGV sont accessibles à
+      tout moment sur le site https://antonime.fr et prévaudront sur tout autre document. Le client
+      déclare avoir pris connaissance des présentes CGV et les avoir acceptées en cochant la
+      case prévue à cet effet avant la mise en œuvre de la procédure de commande en ligne du
+      site https://antonime.fr. Sauf preuve contraire, les données enregistrées dans le système
+      informatique du Prestataire constituent la preuve de l’ensemble des transactions conclues
+      avec le Client.
+    </p>
+    <br>
+    <p>Les coordonnées du Prestataire sont les suivantes :</p>
+    <br>
+    <p>
+      WARSHEEP STUDIO
+      <br>
+      6 av. François Arago
+      <br>
+      92160 Antony
+      <br>
+      Numéro d’immatriculation: W921005727
+      <br>
+      Mail: warsheepstudio.contact@gmail.com
+      <br>
+      Téléphone: 06 82 47 37 19
+    </p>
+    <br>
+    <p>Des droits de douane ou autres taxes locales ou droits d’importation ou taxes d’état sont
+      susceptibles d’être exigibles. Ils seront à la charge et relèvent de la seule responsabilité du
+      Client.
+    </p>
+    
+    <h3 class="text-2xl font-bold py-8">ARTICLE 2 - PRIX</h3>
+      <p>Les services sont fournis aux tarifs en vigueur figurant sur le site https://antonime.fr, lors de
+        l’enregistrement de la commande par le Prestataire.</p>
+      <br>
+      <p>Les prix sont exprimés en euros, TTC.</p>
+      <br>
+      <p>Les tarifs tiennent compte d’éventuelles réductions qui seraient consenties par le Prestataire
+        sur le site https://antonime.fr.</p>
+      <br>
+      <p>Ces tarifs sont fermes et non révisables pendant leur période de validité mais le Prestataire
+        se réserve le droit, hors période de validité, d’en modifier les prix à tout moment.</p>
+      <br>
+      <p>Les prix ne comprennent pas les frais de traitement, d’expédition, de transport et de
+        livraison, qui sont facturés en supplément, dans les conditions indiquées sur le site et
+        calculés préalablement à la passation de la commande.</p>
+      <br>
+      <p>Le paiement demandé au Client correspond au montant total de l’achat, y compris ces frais.</p>
+      <br>
+      <p>Une facture est établie par le Prestataire et remise au Client lors de la fourniture des Service
+        commandés.</p>
+
+    
+    <h3 class="text-2xl font-bold py-8">ARTICLE 3 - COMMANDES</h3>
+
+    
+    <h3 class="text-2xl font-bold py-8">ARTICLE 4 - CONDITIONS DE PAIEMENT</h3>
+
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 5 - FOURNITURE DES PRESTATIONS</h3>
+
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 6 - ANNULATION, REMBOURSEMENT, UTILISATION</h3>
+    
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 7 - RESPONSABILITÉ DU PRESTATAIRE ET GARANTIES</h3>
+
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 8 - DONNEES PERSONNELLES</h3>
+    
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 9 - PROPRIETE INTELLECTUELLE</h3>
+    
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 10 - DROIT APPLICABLE - LANGUE</h3>
+
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 11 - LITIGES</h3>
+
+
+    <h3 class="text-2xl font-bold py-8">ARTICLE 12 - CONDITIONS D'ACCÈS À LA MANIFESTATION</h3>
+
   </div>
 
-  <MainFooter/>
+  <MainFooter />
 </template>
 
 <script>
-  import MainFooter from '@/components/MainFooter.vue';
-  import HeaderTitle from '../components/HeaderTitle.vue';
+import MainFooter from '@/components/MainFooter.vue';
+import HeaderTitle from '../components/HeaderTitle.vue';
 
-  export default {
-    components: {
-      MainFooter,
-      HeaderTitle
-    }
+export default {
+  components: {
+    MainFooter,
+    HeaderTitle
   }
+}
 </script>
