7b27e70f2c703fb22ccc8799bdc646c4d4d487d0
Nico <dbz0793@gmail.com>
2024-07-28 22:48:50 +0200

    [FEAT] -- Update Bar page

--- src/views/BarView.vue
+++ src/views/BarView.vue
@@ -2,7 +2,11 @@
   <HeaderTitle title="Bar" />
   
   <div class="container my-7 px-7">
-    <p>Hello</p>
+
+    <ImageTextGrid :isInverted="true" imageGrid="Bar2.webp" paragraphe="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac,
+    sagittis bibendum sapien. Integer molestie nulla eget placerat auctor. Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit. Donec neque libero, fermentum nec tempus ac, sagittis bibendum sapien.
+    Integer molestie nulla eget placerat auctor."/>
   </div>
   
   <MainFooter/>
@@ -10,11 +14,13 @@
   
 <script>
   import MainFooter from '@/components/MainFooter.vue';
+  import ImageTextGrid from '@/components/ImageTextGrid.vue';
   import HeaderTitle from '@/components/HeaderTitle.vue';
 
   export default {
     components: {
       MainFooter,
+      ImageTextGrid,
       HeaderTitle
     }
   };
