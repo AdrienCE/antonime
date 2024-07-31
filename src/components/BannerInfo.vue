5b3b3dc38c5d462e5848244a6b0c3f3064444436
Nico <dbz0793@gmail.com>
2024-07-31 22:34:34 +0200

    [HOTFIX] -- Update link issue in BannerInfo

--- src/components/BannerInfo.vue
+++ src/components/BannerInfo.vue
@@ -1,10 +1,10 @@
 <template>
-  <a href="/billeterie" class="text-white bg-custom-salmon flex items-center justify-center rounded-md p-7 my-16 md:h-[250px]">
+  <router-link to="/billeterie" class="text-white bg-custom-salmon flex items-center justify-center rounded-md p-7 my-16 md:h-[250px]">
     <div class="text-center text-custom-red">
       <h2 class="font-bold text-2xl mb-5 xl:text-4xl">Billeterie ouverte !</h2>
       <p class="text-base xl:text-2xl">Venez prendre votre billet<br/>au plus vite !</p>
     </div>
-  </a>
+  </router-link>
 </template>
 
 <script setup>
