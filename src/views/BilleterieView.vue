dc54159b251dac93fcf5836a3da45c6b4817e30e
Nico <dbz0793@gmail.com>
2024-08-24 23:15:12 +0200

    [FEAT] -- Expo update; image guest update; add new wording for billeterie

--- src/views/BilleterieView.vue
+++ src/views/BilleterieView.vue
@@ -2,6 +2,9 @@
   <HeaderTitle title="Billetterie" />
   
   <div class="container my-7 px-7">
+
+    <p class="my-7">Retrouver nos options MAID CAFE ou NAVETTE à la prochaine étape, une fois votre billet sélectionné !</p>
+
     <iframe id="haWidget" allowtransparency="true" scrolling="auto" src="https://www.helloasso.com/associations/warsheep-studio/evenements/billeteries-conventions-anto-nime/widget" style="width: 100%; height: 750px; border: none;"></iframe>
   </div>
 
